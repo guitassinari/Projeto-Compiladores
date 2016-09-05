5412ce3d55b157606ffb24e58e33361637b283ac
Caroline Knorr <knorr.makeup@gmail.com>
2016-09-05 11:22:33 -0300

    Adds arguments to command line execution

--- etapa2/main.c
+++ etapa2/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(){
-  initMe();
-  extern FILE *yyin;
-  yyin = fopen("teste.txt","r");
-  yyparse();
+void main(int argc, char *argv[]){
+	initMe();
+
+	if(argc < 2){
+		printf("Insira um arquivo para ser analisado!\n");
+		exit(3);
+	}
+
+	extern FILE *yyin;
+	yyin = fopen(argv[1],"r");
+	yyparse();
 }
